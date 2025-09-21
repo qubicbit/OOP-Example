27cd2fa90a0e3f0ae2139f2c7b51e55a8169c2b1
qubicbit <sis.esse161@outlook.com>
2025-09-22 00:28:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of Shape
+            Shape shape1 = new Shape();
+            shape1.Name = "Circle";
+            shape1.IsFilled = false;
+            shape1.GetInfo(); // Output: This is a Circle and it Not filled.
+
+            // create another instance of Shape
+            Shape shape2 = new Shape();
+            shape2.Name = "Rectangle";
+            shape2.IsFilled = true;
+            shape2.GetInfo(); // Output: This is a Rectangle and it is Filled.
+
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,16 @@
+﻿namespace OOP_Example
+{
+    public class Shape
+    {
+        // Properties
+        public string Name { get; set; }
+        public bool IsFilled { get; set; }
+
+        // Methods
+        public void GetInfo()
+        {
+            string fillStatus = IsFilled ? "Filled" : "Not filled"; //Derived value
+            Console.WriteLine($"This is a {Name} and it is {fillStatus}.");
+        }
+    }
+}
